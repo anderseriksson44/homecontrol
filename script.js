596fabcbac138eb90ab0826a266043b6281c625c
Anders Eriksson <a.eriksson44@gmail.com>
2016-11-25 14:10:22 +0100

    new updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* jQuery function to fade boxes at hover */
 $(document).ready(function() {
     $(".box").on("mouseenter", function(){
         $(this).fadeTo("fast", 0.4);
@@ -5,55 +6,129 @@ $(document).ready(function() {
                 $(this).fadeTo("fast", 1);
             });
         });
-    $(".plusbox").click(function(){
-        var domElement = $('<div class="col-md-4 col-sm-6"><div class="rumbox"></div></div>');
-        $(this).before(domElement);
-    });
-    
+
+/* jQuery function to to add a new light */
+    var lightcount = 5;
+    $("#addlight").on("click", function(){
         
-  $( function() {
-    $( "#slider" ).slider();
-  } );
-  
+        var domElement = $('<div class="col-md-3 col-sm-6 own text-center"><div id="L' + lightcount +'" class="rumbox"></div><button id="L' + lightcount +'" type="button" class="lampknapp btn btn-md btn-success">  ON  </button></div>');
+//        var lid = $(domElement).attr("id");
 
-
-$(function() {
-      $("#toggle-one").bootstrapToggle(function(){
-        $(this).change(function() {
-            $("rum1").addClass("rumbox-light");
-        });
+        //Add it to the DOM
+        $("#aa").append(domElement)
+        console.log(domElement);
+//        var lid= (domElement).attr("id");
+//        console.log(lid);
+//        
+//        $("#"+lid).after(domElement);
+        lightcount++;
     });
+    
+
+
+/* jQuery plugin-function for a nice toggle-switch */
+//$(function() {
+//      $("#toggle").change(function(){
+//          console.log("hej");
+//        $("#rum").css("background-color", "yellow");
+//        });
+//    });
+    
+/* Eventlistener for confirmation when adding a new room to the list */
+
+var rum = document.getElementById("addroom");
+rum.addEventListener("click", addaRoom, false);    
+    
+
+/* Function that list called when a new room should be added to the tab-list*/
+
+function addaRoom() {
+var roomName = document.getElementById('rum').value; /* store name of room */
+var result = document.getElementById('result')
+
+if (roomName.length > 10) {
+    result.textContent = "För långt namn (max 10 tecken)";
+    alert("För långt namn (max 10 tecken)");
+} else {
+    var ul = document.getElementById("rumlista");
+    var list = document.createElement("li");
+    ul.appendChild(list);
+    
+    var atag = document.createElement("a");
+    atag.setAttribute("href", "#6");
+    atag.setAttribute("data-toggle", "tab");
+    atag.innerHTML= (roomName);
+    
+    list.appendChild(atag);
+    
+    
+        }
+}
+    
+
+//var knapp = lid.querySelectorAll("#id");
+//console.log(knapp);    
+
+
+// Turn the light on and off    
+
+
+    $("#L2").on("click", function(){
+        var onoff = $(this).attr("id");
+        var rumlampa = $(this).siblings("div");
+        console.log(onoff);
+        console.log(rumlampa);
+        $(rumlampa).toggleClass("rumbox-light");
 });
-    
-function nyttRum() {
-        alert("Hej");
-//        var node = document.createElement("li");
-//        var textnode = document.createTextNode(text);
-//        node.appendChild(textnode);
-//        document.getElementById("exTab1").appendChild(node);
-};
 
+    var on ="true";
+$(".lampknapp").on("click", function () {
+        var onoff = $(this).attr("id");
+        var rumlampa = $(this).siblings("div").attr("id");
+        console.log(onoff);
+        console.log(rumlampa);
+        
+        if (onoff==rumlampa) {
+            $("#"+rumlampa).toggleClass("rumbox-light");
+        }
     
-var onoff = document.querySelector("#onoff");
-var lampa = document.querySelector("#rum2");
-var tand = true;
+        if  (on == "true") {
+            $(this).addClass("btn-danger");
+            $(this).html("OFF");
+            on ="false";
+        }
+        else {
+            $(this).removeClass("btn-danger");
+            $(this).html("ON");
+            on ="true";
+        }   
+        
+    });
     
-onoff.addEventListener("click", function(){
-    lampa.classList.toggle("rumbox-light");
     
-    if (tand == true) {
-        this.classList.add("btn-danger");
-        this.innerHTML = "OFF";
-        tand = false;
-    }
-    else {
-        this.innerHTML = "ON";
-        this.classList.toggle("btn-warning");
-        tand = true; 
-        console.log(this.classList);
-    }
+//    
+//var onoff = document.querySelector("#onoff");
+//var lampa = document.querySelector("#rum2");
+//var tand = true;
+//    
+//onoff.addEventListener("click", function(){
+//    lampa.classList.toggle("rumbox-light");
+//    
+//    if (tand == true) {
+//        this.classList.add("btn-danger");
+//        this.innerHTML = "OFF";
+//        tand = false;
+//    }
+//    else {
+//        this.classList.remove("btn-danger");
+//        this.classList.add("btn-success");
+//        this.innerHTML = "ON";
+//        tand = true; 
+//        
+//    }
+//
+//    }); 
 
-    }); 
 
 
 });
